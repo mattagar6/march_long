310f1ac93d08901a4d87e653c4bff571b6f0c07e
mattagar6 <mattagar6@gmail.com>
2020-03-10 23:12:14 -0400

    struggling with f

--- brute_f.cpp
+++ brute_f.cpp
@@ -1,42 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void sub_1() {
-	int n;
-	cin >> n;
-	vector<int> a(n), b(n);
-	for(int& x : a) cin >> x;
-	for(int& x : b) cin >> x;
-	sort(a.begin(), a.end());
-	sort(b.begin(), b.end());
-	set<int> seen;
-	bool ok = true;
-	for(int i = 0; i < n; i++) {
-		if(b[i] > a[i]) {
-			if(i > 0 && seen.count(a[i]) == 0) {
-				ok = false;
-			}
-		} else {
-			ok = false;
-		}
-		seen.insert(a[i]);
-		seen.insert(b[i]);
+bool rec(vector<int> a, vector<int> b, map<int, int> table, bool attack, int beat) {
+	if(a.empty() && b.empty()) {
+		return true;
 	}
-	
-	cout << (ok ? "YES" : "NO") << '\n';
+
+	if(a.empty() && attack) {
+		return false;
+	}
+
+	if(b.empty() && !attack) {
+		return false;
+	}
+
+	int n = a.size(), m = b.size();
+	if(attack) {
+		if(table.empty()) {
+			for(int i = 0; i < n; i++) {
+				// try placing this card
+				vector<int> next;
+				for(int j = 0; j < n; j++) {
+					if(i == j) continue;
+					next.push_back(a[j]);
+				}
+
+				table[a[i]]++;
+				if(rec(next, b, table, false, a[i])) {
+					return true;
+				}
+				table[a[i]]--;
+				if(table[a[i]] == 0) {
+					table.erase(a[i]);
+				}
+			}
+
+			return rec(b, a, map<int, int>(), true, -1);
+		} else {
+			for(int i = 0; i < n; i++) {
+				if(table[count[a[i]]] == 0) continue;
+				vector<int> next;
+				for(int j = 0; j < n; j++) {
+					if(i == j) continue;
+					next.push_back(a[j]);
+				}
+
+				table[a[i]]++;
+				if(rec(next, b, table, false, a[i])) {
+					return true;
+				}
+				table[a[i]]--;
+				if(table[a[i]] == 0) {
+					table.erase(a[i]);
+				}
+			}
+
+			return rec(b, a, map<int, int>(), true, -1);
+		}
+	} else {
+		for(int j = 0; j < m; j++) {
+
+		}
+	}
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
+	
+	/*	make all swaps at the beginning,
+	 *	then same as subtask 1
+	 *
+	 *	is order of moves relevant? kindof
+	 */
 
-	int tc, s;
-	cin >> tc >> s;
-	while(tc--) {
-		if(s == 1) {
-			sub_1();
-		} else {
-			
-		}
-	}
 }
